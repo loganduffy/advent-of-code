9d0d527b24c597cde2cb3e63ca50b3b6cc1e787e
logan <logan.duffy@ucalgary.ca>
2025-12-13 15:30:36 -0700

    Finished part one for day 2

--- day2/part-one/part-one.cpp
+++ day2/part-one/part-one.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <cstdint>
 #include <iostream>
 #include <cmath>
 #include <string>
@@ -23,20 +25,70 @@ std::vector<std::string> readFile (const std::string& filename)
   {
     if (!range.empty())
     {
+      range.erase(range.find_last_not_of(" \t\n\r\f\v") + 1);
+      range.erase(0, range.find_first_not_of(" \t\n\r\f\v"));
       idRanges.push_back(range);
     }
   }
   return idRanges;
 }
 
+
+uint64_t invalidIDs(uint64_t L, uint64_t R)
+{
+  uint64_t sum = 0;
+
+  for (int halfLength = 1; halfLength <= 9; halfLength++)
+  {
+    uint64_t minHalf = 1;
+    for (int i = 1; i < halfLength; i++) minHalf *= 10;
+
+    uint64_t maxHalf = minHalf * 10 - 1;
+
+    for (uint64_t firstHalf = minHalf; firstHalf <= maxHalf; firstHalf++)
+    {
+
+      std::string firstStr = std::to_string(firstHalf);
+      std::string invalidStr = firstStr + firstStr;
+      uint64_t invalidID;
+
+
+      try
+      {
+        invalidID = std::stoul(invalidStr);
+      }
+      catch (...)
+      {
+        break;
+      }
+
+      if (invalidID < L) continue;
+      if (invalidID > R) break;
+
+      sum += invalidID;
+
+    }
+  }
+
+  return sum;
+}
+
 int main()
 {
   std::vector<std::string> idRanges = readFile("../puzzleranges.txt");
+  uint64_t totalSum = 0;
 
-  for (size_t i = 0; i < idRanges.size(); i++)
+  for (const auto& rangeStr : idRanges)
   {
-       std::cout << idRanges[i] << std::endl;
+    size_t dashPosition = rangeStr.find('-');
+    uint64_t start = std::stoul(rangeStr.substr(0, dashPosition));
+    uint64_t end = std::stoul(rangeStr.substr(dashPosition + 1));
+    uint64_t rangeSum = invalidIDs(start, end);
+    totalSum += rangeSum;
   }
 
-  return 0; 
+  std::cout << totalSum << std::endl;
+
+  return 0;
 }
+
